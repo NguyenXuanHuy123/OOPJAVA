2c81694e8ba76a5aca93ac720b3b0bcbfec59568
huy.nguyen <yasuo01665601635@gmail.com>
2024-01-24 21:58:48 +0700

    Create bai30.java

---
+++ BaiThucHanh/src/bai30.java
@@ -0,0 +1,18 @@
+import java.util.Scanner;
+
+public class bai30 {
+    public static void main(String[] args) {
+     Scanner sc = new Scanner(System.in);
+     System.out.println("nhap n:");
+     int n = sc.nextInt();
+     int sum = 0;
+     int du;
+     while (n>0) {
+        du = n % 10;
+        n = n / 10;
+        sum = sum + du;
+     }
+     System.out.println("tong = " +sum);
+     sc.close();
+    }
+}
