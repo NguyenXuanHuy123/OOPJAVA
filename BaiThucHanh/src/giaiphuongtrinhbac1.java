f6ed72ac8752fee1c09321eba1fe179f78a04402
huy.nguyen <yasuo01665601635@gmail.com>
2024-01-24 21:58:40 +0700

    Create giaiphuongtrinhbac1.java

---
+++ BaiThucHanh/src/giaiphuongtrinhbac1.java
@@ -0,0 +1,25 @@
+import java.util.Scanner;
+
+public class giaiphuongtrinhbac1 {
+     public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+         System.out.println("nhap a:");
+         Double a = sc.nextDouble();
+         System.out.println("nhap b:");
+         Double b = sc.nextDouble();
+         Double x;
+         System.out.println("Phuong trinh nhap vao la : " +a +"x" + b + "= 0");
+         if (a==0){
+            if(b==0){
+                System.out.println("Phuong trinh vo so nghiem");
+                }else{
+                System.out.println("phuong trinh vo nghiem");
+            }
+                }else{
+            x = -b/a;
+            System.out.println("phuong trinh co nghiem la x =" + x);
+         }
+    sc.close();
+
+     }
+}
