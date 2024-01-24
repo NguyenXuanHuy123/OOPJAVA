760d8fb960ba357d1b55f261eaa13fe3d158a95f
huy.nguyen <yasuo01665601635@gmail.com>
2024-01-24 21:59:08 +0700

    Create bai35.java

---
+++ BaiThucHanh/src/bai35.java
@@ -0,0 +1,18 @@
+import java.util.Scanner;
+
+public class bai35 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.println("nhap so1:");
+        int so1 = sc.nextInt();
+        System.out.println("nhap so2:");
+        int so2 = sc.nextInt();
+        if(so1<so2){
+            System.out.println("so nho nhat la : " +so1);
+        }else{
+            System.out.println("so nho nhat la : " +so2);
+            sc.close();
+        }
+    }
+}
+
