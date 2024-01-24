2b6f23465ade6140324323bf5cff3289e1745742
huy.nguyen <yasuo01665601635@gmail.com>
2024-01-24 21:59:17 +0700

    Create bai47.java

---
+++ BaiThucHanh/src/bai47.java
@@ -0,0 +1,17 @@
+import java.util.Scanner;
+
+public class bai47 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+         int n;
+         int tong = 0;
+         do{
+            System.out.println("nhap n: ");
+            n = sc.nextInt();
+            tong = tong + n;
+         }while(tong<100);
+         System.out.println("tong cua cac so nguyen = " +tong);
+         sc.close();
+    }
+}
+
