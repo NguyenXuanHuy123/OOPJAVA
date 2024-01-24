31b0a4ea78e24a7d381e2dc66e7e46f4cb794d9a
huy.nguyen <yasuo01665601635@gmail.com>
2024-01-24 21:58:44 +0700

    Create giaiphuongtrinhbac2.java

---
+++ BaiThucHanh/src/giaiphuongtrinhbac2.java
@@ -0,0 +1,35 @@
+import java.util.Scanner;
+
+public class giaiphuongtrinhbac2 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.println("nhap a:");
+        Double a = sc.nextDouble();
+        System.out.println("nhap b:");
+        Double b = sc.nextDouble();
+        System.out.println("nhap c:");
+        Double c = sc.nextDouble();
+        Double dental;
+        if (a==0) {
+            if(b==0){
+                System.out.println("phuong trinh vo nghiem");       
+        }else{
+            System.out.println("phuong trinh co mot nghiem x =" + (-c/b));
+            sc.close();
+        } return;}
+         dental = b*b -4*(a*c);
+        if(dental<0){
+         System.out.println("phuong trinh vo nghiem");
+         }else{
+            if(dental==0){
+                System.out.println("phuong trinh co nghiem kep x1=x2= " +(-b/2*a));
+            }else{
+             System.out.println("phuong trinh co nghiem x1= " +(-b +Math.sqrt(dental)/2*a));
+             System.out.println("phuong trinh co nghiem x2= " +(-b -Math.sqrt(dental)/2*a));
+            }
+            sc.close();
+         }
+         }
+
+
+    }
