bc28a3e87af6a16dd320872bb9394b75282b857c
huy.nguyen <yasuo01665601635@gmail.com>
2024-01-31 11:26:40 +0700

    Create bai74.java

---
+++ BaiThucHanh/src/bai74.java
@@ -0,0 +1,25 @@
+import java.util.Scanner;
+
+public class bai74 {
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int n;
+          do{
+        System.out.println("nhap so phan tu mang : ");
+         n = sc.nextInt();
+        }while(n<0);
+         int B[] = new int[n];    
+         for(int i=0;i<n;i++){
+         System.out.println("nhap phan tu thu " +i+ ":");
+         B[i] = sc.nextInt();
+         }
+         double tong = 0;
+         for(int i=0;i<n;i++){
+            if(B[i] % 2 ==0)
+            tong = tong + B[i];
+         }
+        System.out.println("gia tri cua mang la :" +tong);
+        sc.close();
+    }
+}
