80dc5068a3e68fd7b1a109bc306de42e5b151d47
huy.nguyen <yasuo01665601635@gmail.com>
2024-01-31 11:26:45 +0700

    Create bai90.java

---
+++ BaiThucHanh/src/bai90.java
@@ -0,0 +1,18 @@
+import java.util.Scanner;
+
+public class bai90 {
+    
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        String chuoi;
+        char kytu;
+        System.out.println("nhap chuoi : ");
+        chuoi = sc.nextLine();
+        System.err.println("cac ky tu co trong chuoi la : ");
+        for(int i=0;i<chuoi.length();i++){
+            kytu = chuoi.charAt(i);
+            System.out.println(kytu);
+        }
+        sc.close();
+    }
+}
