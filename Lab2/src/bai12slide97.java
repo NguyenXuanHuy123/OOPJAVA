3feb6ce792011805dec9e8d81ee5ac63e6df9f0a
huy.nguyen <yasuo01665601635@gmail.com>
2024-02-01 22:34:26 +0700

    Create bai12slide97.java

---
+++ Lab2/src/bai12slide97.java
@@ -0,0 +1,34 @@
+import java.util.Scanner;
+
+public class bai12slide97 {
+
+    public static void main(String[] args) {
+         Scanner sc = new Scanner(System.in);
+         int m,n;
+         System.out.println("nhap so cot :" );
+         m = sc.nextInt();
+         System.out.println("nhap so dong : ");
+         n = sc.nextInt();
+         int arr[][] = new int[m][n];
+         System.out.println("nhap so phan tu trong mang : ");
+         for(int i=0;i<m;i++){
+            for(int j=0;j<n;j++){
+                System.out.println("arr [" +i+ "] [" +j+ "] = ");
+                arr[i][j] = sc.nextInt();
+            }
+         }
+         int max = arr[0][0];
+         for(int i=0;i<m;i++){
+            for(int j=0;j<n;j++){
+                if(max < arr[i][j]){
+                    max=arr[i][j];
+            
+    
+            }
+         }
+
+    }
+         System.out.println("phan tu co gia tri lon nhat trong ma tran la : " +max);
+         sc.close();
+}
+}
