539340e046d19d5a75f3dbe1b24e29ea2f5181e2
huy.nguyen <yasuo01665601635@gmail.com>
2024-03-05 19:15:54 +0700

    Create HinhTron.java

---
+++ BaiThucHanh/src/HinhTron.java
@@ -0,0 +1,24 @@
+import java.util.Scanner;
+
+public class HinhTron {
+    final float Pi = 3.14f;
+    public float BanKinh;
+    public float ChuVi;
+    public float DienTich;
+    public void nhapBanKinh(){
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Nhap R = ");
+        BanKinh = sc.nextFloat();
+        sc.close();
+    }
+    public void nhapChuvi(){
+        ChuVi = 2 * Pi * BanKinh;
+    }
+    public void nhapDienTich(){
+        DienTich = Pi * BanKinh * BanKinh;
+    }
+    public void inramanhinh(){
+        System.out.println("\nChu vi hinh tron la : " + ChuVi);
+        System.out.println("\nDien tich hinh tron la : " + DienTich);
+    }
+}
