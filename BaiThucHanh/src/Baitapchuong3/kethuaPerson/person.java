abe623b79409927a4cd4919f3376289548d9cfbb
huy.nguyen <yasuo01665601635@gmail.com>
2024-03-21 20:14:36 +0700

    Create person.java

---
+++ BaiThucHanh/src/Baitapchuong3/kethuaPerson/person.java
@@ -0,0 +1,24 @@
+package Baitapchuong3.KethuaHinhhoc.kethuaPerson;
+
+import java.util.Scanner;
+
+public class person {
+    public String ten;
+    public String gioitinh;
+    public int tuoi;
+
+    public void nhap(){
+        Scanner sc = new Scanner(System.in);
+        System.out.print("nhap ten : ");
+        ten = sc.nextLine();
+        System.out.print("nhap gioi tinh : ");
+        gioitinh = sc.nextLine();
+        System.out.print("nhap tuoi : ");
+        tuoi = sc.nextInt();
+    }
+    public void In(){
+        System.out.println("ho va ten : " +ten);
+        System.out.println("gioi tinh : " +gioitinh);
+        System.out.println("tuoi : " +tuoi);
+    }
+}
