0221f61d704117c69a539fa4e2104c97748758f2
huy.nguyen <yasuo01665601635@gmail.com>
2024-03-21 20:14:29 +0700

    Create employeefulltime.java

---
+++ BaiThucHanh/src/Baitapchuong3/kethuaPerson/employeefulltime.java
@@ -0,0 +1,27 @@
+package Baitapchuong3.KethuaHinhhoc.kethuaPerson;
+
+import java.util.Scanner;
+
+public class employeefulltime extends employee {
+    public int ngaycong;
+
+    public employeefulltime(){
+        ten = "nhan vien fulltime";
+        luong = 800000;
+    }
+
+    public void nhapngaycong(){
+        super.nhapmnv();
+        Scanner sc = new Scanner(System.in);
+        System.out.println("nhap ngay cong : ");
+        ngaycong = sc.nextInt();
+    }
+    public void tinhluong(){
+        luong = luong *ngaycong;
+    }
+    public void inngaycong(){
+        super.inmvn();
+        System.out.println("Luong cua nhan vien fulltime : " + luong);
+    }
+    
+}
