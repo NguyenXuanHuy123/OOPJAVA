6cb8a14b6b8431521e04629e8c613f0cb80cca9f
huy.nguyen <yasuo01665601635@gmail.com>
2024-03-21 20:14:32 +0700

    Create employeeparttime.java

---
+++ BaiThucHanh/src/Baitapchuong3/kethuaPerson/employeeparttime.java
@@ -0,0 +1,26 @@
+package Baitapchuong3.KethuaHinhhoc.kethuaPerson;
+
+import java.util.Scanner;
+
+public class employeeparttime extends employee {
+    public int giolamviec;
+
+    public employeeparttime(){
+        ten = "nhan vien part time";
+        luong = 300000;
+    }
+    public void nhapgiolamviec(){
+        super.nhapmnv();
+        Scanner sc = new Scanner(System.in);
+        System.out.println("nhap so gio lam viec : ");
+        giolamviec = sc.nextInt();
+    }
+    public void tinhluong(){
+        luong = luong * giolamviec;
+    }
+
+    public void ingiolamviec(){
+        super.inmvn();
+        System.out.println("luong cua nhan vien parttime = " +  luong);
+    }
+}
