0b05cec96891af70975103cf90365a523c36bf0a
huy.nguyen <yasuo01665601635@gmail.com>
2024-03-21 20:14:24 +0700

    Create Hinhvuong.java

---
+++ BaiThucHanh/src/Baitapchuong3/KethuaHinhhoc/Hinhvuong.java
@@ -0,0 +1,10 @@
+package Baitapchuong3.KethuaHinhhoc;
+
+public class Hinhvuong extends Hinhchunhat{
+    public Hinhvuong(){
+        ten = "Hinhvuong";
+    }
+    public void nhapcanh(){
+        super.nhap();
+    }
+}
