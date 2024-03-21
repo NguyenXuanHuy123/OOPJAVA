66d1eda29d17df3e6015847dc0cb947b53dfccbe
huy.nguyen <yasuo01665601635@gmail.com>
2024-03-21 20:13:57 +0700

    Create Hinhhoc.java

---
+++ BaiThucHanh/src/Baitapchuong3/KethuaHinhhoc/Hinhhoc.java
@@ -0,0 +1,23 @@
+package Baitapchuong3.KethuaHinhhoc;
+
+public class Hinhhoc {
+    public final float Pi = 3.14f;
+    public String ten;
+    public float chuvi;
+    public float dientich;
+    public float thetich;
+
+    public Hinhhoc(){}
+    public void Xuatten(){
+        System.out.println("ten hinh la : " + ten);
+    }
+    public void inchuvi(){
+        System.out.println("chu vi = " +chuvi);
+    }
+    public void indientich(){
+        System.out.println("dien tich = " +dientich);
+    }
+    public void inthetich(){
+        System.out.println("the tich  = " +thetich);
+    }
+}
