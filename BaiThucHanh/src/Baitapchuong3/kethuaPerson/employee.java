94a77c47c9799729dcb657508a733ca2b17ce884
huy.nguyen <yasuo01665601635@gmail.com>
2024-03-21 20:14:27 +0700

    Create employee.java

---
+++ BaiThucHanh/src/Baitapchuong3/kethuaPerson/employee.java
@@ -0,0 +1,18 @@
+package Baitapchuong3.KethuaHinhhoc.kethuaPerson;
+
+import java.util.Scanner;
+
+public class employee extends person {
+    public String mnv;
+    public long luong;
+    public void nhapmnv(){
+        super.nhap();
+        Scanner sc = new Scanner(System.in);
+        System.out.println("nhap ma nhan vien : ");
+        mnv = sc.nextLine();
+    }
+    public void inmvn(){
+        super.In();
+        System.out.println("ma nhan vien la: " + mnv);
+    }
+}
