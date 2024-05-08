78dc0c9b2ee58c3302f518b3ce18819c71236b6d
huy.nguyen <yasuo01665601635@gmail.com>
2024-05-08 09:00:08 +0700

    Create configs.java

---
+++ BaiThucHanh/src/Baitapchuong3/Baitapslide79/configs.java
@@ -0,0 +1,11 @@
+
+
+public class configs {
+
+    public static final int sep = 1;
+    public static final int linh = 2;
+    public static final long Luong_sep = 20000000;
+    public static final long Luong_linh = 10000000;
+    public static final long luong_lam_them_moi_ngay = 800000;
+    public static final long luong_lam_them_moi_gio = 100000;
+}
